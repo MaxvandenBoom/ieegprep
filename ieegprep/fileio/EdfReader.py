7d22f3ea6ea13e84c979d0a90d63ab1dc3fd0ef7
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2023-02-15 16:22:23 -0600

    Formatted headers

--- ieegprep/fileio/EdfReader.py
+++ ieegprep/fileio/EdfReader.py
@@ -1,18 +1,15 @@
 """
 Functions to read European Data Format (EDF) files
+
+
 =====================================================
-
-
 Copyright 2022, Max van den Boom (Multimodal Neuroimaging Lab, Mayo Clinic, Rochester MN)
 
 Adapted from Fieldtrip (by Robert Robert Oostenveld) while replicating some additional header logic from the MNE
 package (Teon Brooks, Martin Billinger, Nicolas Barascud, Stefan Appelhoff, Joan Massich, Clemens Brunner, Jeroen Van Der Donckt)
 
-
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import sys
