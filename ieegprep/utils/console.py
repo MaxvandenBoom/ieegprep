d67ebe2ae6b58be32816457f3a587a5351dc2485
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2023-02-15 16:24:56 -0600

    Moved clear cache function; formatted headers

--- ieegprep/utils/console.py
+++ ieegprep/utils/console.py
@@ -1,13 +1,11 @@
 """
 Functions to format and color console text
-=====================================================
 
+=====================================================
 Copyright 2023, Max van den Boom (Multimodal Neuroimaging Lab, Mayo Clinic, Rochester MN)
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import logging
@@ -56,6 +54,10 @@ class ConsoleColors:
     @staticmethod
     def print_color(color, text):
         print(color + text + ConsoleColors.RESET)
+
+    @staticmethod
+    def print_green(text):
+        print(ConsoleColors.FG.GREEN + text + ConsoleColors.RESET)
 
     @staticmethod
     def print_error(text):
