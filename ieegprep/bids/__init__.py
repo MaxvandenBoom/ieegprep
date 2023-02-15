d4eb80dbbb04fe911d2813bd090b4cd6037a1db3
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2023-02-15 17:42:52 -0600

    Added init files for packages and access

---
+++ ieegprep/bids/__init__.py
@@ -0,0 +1,7 @@
+# define directory as package
+
+# flatten access
+#from data_structure import index_bids_datasets
+from sidecars import load_channel_info, load_event_info, load_ieeg_sidecar
+#from data_epoch import load_data_epochs, load_data_epochs_averages
+
