326691bc4160f3e5306f5a768294d383080ce5fd
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2023-03-01 16:30:38 -0600

    Absolute namespacing

--- ieegprep/bids/__init__.py
+++ ieegprep/bids/__init__.py
@@ -2,6 +2,6 @@
 
 # flatten access
 #from data_structure import index_bids_datasets
-from sidecars import load_channel_info, load_event_info, load_ieeg_sidecar
+from ieegprep.bids.sidecars import load_channel_info, load_event_info, load_ieeg_sidecar
 #from data_epoch import load_data_epochs, load_data_epochs_averages
 
