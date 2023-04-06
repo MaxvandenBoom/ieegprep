084c3c0ada4ebd652c4822510a1b9227edd24b7e
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2023-04-06 16:14:44 -0500

    Added flattened access to list bids datasets

--- ieegprep/bids/__init__.py
+++ ieegprep/bids/__init__.py
@@ -1,7 +1,7 @@
 # define directory as package
 
 # flatten access
-#from data_structure import index_bids_datasets
+from ieegprep.bids.data_structure import list_bids_datasets
 from ieegprep.bids.sidecars import load_channel_info, load_event_info, load_ieeg_sidecar
 #from data_epoch import load_data_epochs, load_data_epochs_averages
 
