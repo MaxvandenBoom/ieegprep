e9156bdc58b4ec37c9a109af715d32d1a712f4f1
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2023-04-10 17:14:13 -0500

    Flattened access to BIDS sidecar functions

--- ieegprep/__init__.py
+++ ieegprep/__init__.py
@@ -9,4 +9,5 @@ if sys.version_info < (3, 8, 0):
 from ieegprep.version import __version__
 from ieegprep.bids.data_epoch import load_data_epochs, load_data_epochs_averages
 from ieegprep.bids.rereferencing import RerefStruct
+from ieegprep.bids.sidecars import load_event_info, load_channel_info, load_ieeg_sidecar
 from ieegprep.fileio.IeegDataReader import VALID_FORMAT_EXTENSIONS, IeegDataReader
